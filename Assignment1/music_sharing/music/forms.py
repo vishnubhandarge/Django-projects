86654ee1c56efee6b0b89cedd97d87957d6cadf4
vishnubhandarge <110363596+vishnubhandarge@users.noreply.github.com>
2023-06-25 10:39:12 +0530

    Delete Assignment1/music_sharing directory

--- Assignment1/music_sharing/music/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import MusicFile
-
-
-class MusicFileForm(forms.ModelForm):
-    class Meta:
-        model = MusicFile
-        fields = ['title', 'file', 'access_type', 'allowed_emails']
